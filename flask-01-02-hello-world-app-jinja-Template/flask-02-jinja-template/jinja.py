ee6de4d818b74c674fdd06c0fd0f5e69a977ec5a
Rofu <emretokac1992@gmail.com>
2022-08-16 13:49:16 +0300

    kdsaf

--- flask-01-02-hello-world-app-jinja-Template/flask-02-jinja-template/jinja.py
+++ flask-01-02-hello-world-app-jinja-Template/flask-02-jinja-template/jinja.py
@@ -2,6 +2,18 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@app.route("/")
+def head():
+    return render_template("index.html")
+
+
+@app.route("/emre")
+def number():
+    num1 = 23
+    num2 = 54
+    return render_template("body.html", num1=num1, num2=num2, sum=num1+num2)
+
+
 
 if __name__== "__main__":
-    app.run(debug=True)
+    app.run(debug=True,port=2000)
